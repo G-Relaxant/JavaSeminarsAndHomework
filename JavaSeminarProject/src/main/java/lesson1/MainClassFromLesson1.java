aa505b18d45b826422aa364952e29f0bc050a539
DanyaKachimati <myclearsoap0369@gmail.com>
2023-04-27 12:35:40 +0400

    edit

---
+++ JavaSeminarProject/src/main/java/lesson1/MainClassFromLesson1.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class MainClassFromLesson1 {
+    public static void main(String[] args) {
+        exOne();
+    }
+
+    private static void exOne() {
+        System.out.println("Hello, world!");
+    }
+}
