855df69b3ec5447e2f30a1e119a5af10ff077707
DanyaKachimati <myclearsoap0369@gmail.com>
2023-04-27 16:33:47 +0400

    ex1-ex4 edit

--- JavaSeminarProject/src/main/java/lesson1/MainClassFromLesson1.java
+++ JavaSeminarProject/src/main/java/lesson1/MainClassFromLesson1.java
@@ -1,5 +1,8 @@
 package lesson1;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class MainClassFromLesson1 {
     public static void main(String[] args) {
         exOne();
@@ -7,5 +10,28 @@ public class MainClassFromLesson1 {
 
     private static void exOne() {
         System.out.println("Hello, world!");
+    }
+
+    private static void exTwo() {
+        Scanner scaner = new Scanner(System.in);
+        String name = scaner.nextLine();
+        System.out.printf("PRIVET, %s", name);
+        scaner.close();             // v dannom sluchae neobyazatelno
+    }
+
+    private static void ex3() {
+        Scanner in = new Scanner(System.in);
+        String name2 = in.nextLine();
+        System.out.printf("Privet, hello, %s%n", name2);
+    }
+
+    private static void ex4() {
+        int a = 1;      //primitiv (sokrashyonniy vizov peremennoy)
+        Integer b = 1;  //rasshirenniy(ssilochniy) vizov peremennoy (mozhno pereyti v klass tipa peremmoy "int" i posmotret parametri) ctrl+leftMouse + click to name ("Integer")
+        List<Integer> list;
+    }
+
+    private static void ex5() {
+
     }
 }
---
+++ JavaSeminarProject/src/main/java/lesson1/homework/Main.java
@@ -0,0 +1,18 @@
+package lesson1.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+//        for (int i = 0; i <; i++) {
+//
+//        }
+//
+//        for (String arg : args) {
+//
+//        }
+
+//        la-la-la
+
+    }
+}
