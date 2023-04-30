f6d71b9b76c3d5c52118d4d7f2e177411710639f
DanyaKachimati <myclearsoap0369@gmail.com>
2023-05-01 01:53:14 +0400

    lesson1_cmplt

--- JavaSeminarProject/src/main/java/lesson1/MainClassFromLesson1.java
+++ JavaSeminarProject/src/main/java/lesson1/MainClassFromLesson1.java
@@ -1,6 +1,8 @@
 package lesson1;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainClassFromLesson1 {
@@ -9,7 +11,10 @@ public class MainClassFromLesson1 {
 //        exTwo();
 //        ex3();
 //        ex4();
-        ex5();
+//        ex5();
+        int rslt = getRandomArrMaxCount(100);
+        System.out.print("Result = ");
+        System.out.println(rslt);
     }
 
     private static void exOne() {
@@ -31,7 +36,7 @@ public class MainClassFromLesson1 {
 
     private static void ex4() {
         int a = 1;      //primitiv (sokrashyonniy vizov peremennoy)
-        Integer b = 1;  //rasshirenniy(ssilochniy) vizov peremennoy (mozhno pereyti v klass tipa peremmoy "int" i posmotret parametri) ctrl+leftMouse + click to name ("Integer")
+        Integer b = 1;  //rasshirenniy(ssilochniy) vizov peremennoy (mozhno pereyti v klass tipa peremennoy "int" i posmotret parametri) ctrl+leftMouse + click to name ("Integer")
         List<Integer> list;
     }
 
@@ -50,5 +55,36 @@ public class MainClassFromLesson1 {
             }
         }
         System.out.printf("Naibolshaya posledovatelnost '1' : %s", result);
+    }
+
+    private static int getRandomArrMaxCount(int size) {
+        int[] arr = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(2); // 0-1
+//            arr[i] = random.nextInt(3,10);  // 3-9
+        }
+
+//        for (int i = 0; i < arr.length; i++) {        // cycle variant 1
+//            int num = arr[i];
+//            System.out.println(num);
+//        }
+
+        int count = 0;
+        int maxCount = 0;
+        for (int num : arr) {                           // cycle variant 2
+            //System.out.println(num);
+            if (num == 1) {
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(arr));
+        return maxCount;
     }
 }
