41cb93f62a4b76b180318ab0f83e20af066380f2
DanyaKachimati <myclearsoap0369@gmail.com>
2023-05-04 20:57:39 +0400

    l1cmplt

---
+++ JavaSeminarProject/src/main/java/lesson6_final/Catalog.java
@@ -0,0 +1,120 @@
+package lesson6_final;
+
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class Catalog {
+
+    lt1.id = 1;
+    lt1.brand = "Lenovo";
+    lt1.price = "35000";
+    lt1.RAM = "8";
+    lt1.ROM = "250";
+    lt1.CPU = "Ryzen 5";
+    lt1.screen = "TN60";
+    lt1.GPU = "Vega 8";
+    lt1.color = "Black";
+
+    Catalog lt2;
+    lt2.id = 2;
+    lt2.brand = "Lenovo";
+    lt2.price = "50000";
+    lt2.RAM = "16";
+    lt2.ROM = "500";
+    lt2.CPU = "Ryzen 5";
+    lt2.screen = "IPS60";
+    lt2.GPU = "Vega 8";
+    lt2.color = "Grey";
+
+    Catalog lt3;
+    lt3.id = 3;
+    lt3.brand = "Lenovo";
+    lt3.price = "80000";
+    lt3.RAM = "16";
+    lt3.ROM = "500";
+    lt3.CPU = "Ryzen 7";
+    lt3.screen = "IPS120";
+    lt3.GPU = "RTX3060";
+    lt3.color = "Black";
+
+    Catalog lt4;
+    lt4.id = 4;
+    lt4.brand = "Samsung";
+    lt4.price = "40000";
+    lt4.RAM = "8";
+    lt4.ROM = "250";
+    lt4.CPU = "Core i5";
+    lt4.screen = "IPS60";
+    lt4.GPU = "IntelUHD";
+    lt4.color = "Grey";
+
+    Catalog lt5;
+    lt5.id = 5;
+    lt5.brand = "Samsung";
+    lt5.price = "50000";
+    lt5.RAM = "16";
+    lt5.ROM = "500";
+    lt5.CPU = "Ryzen 7";
+    lt5.screen = "IPS120";
+    lt5.GPU = "Vega 11";
+    lt5.color = "Black";
+
+    Catalog lt6;
+    lt6.id = 6;
+    lt6.brand = "MSI";
+    lt6.price = "90000";
+    lt6.RAM = "16";
+    lt6.ROM = "500";
+    lt6.CPU = "Ryzen 7";
+    lt6.screen = "IPS144";
+    lt6.GPU = "RTX3070";
+    lt6.color = "White";
+
+    Catalog lt7;
+    lt7.id = 7;
+    lt7.brand = "Asus";
+    lt7.price = "45000";
+    lt7.RAM = "12";
+    lt7.ROM = "250";
+    lt7.CPU = "Core i5";
+    lt7.screen = "IPS60";
+    lt7.GPU = "GTX1650";
+    lt7.color = "Black";
+
+    Catalog lt8 ;
+    lt8.id = 8;
+    lt8.brand = "HP";
+    lt8.price = "30000";
+    lt8.RAM = "8";
+    lt8.ROM = "250";
+    lt8.CPU = "Ryzen 3";
+    lt8.screen = "TN60";
+    lt8.GPU = "Vega 6";
+    lt8.color = "Black";
+
+    Catalog lt9;
+    lt9.id = 9;
+    lt9.brand = "Xiaomi";
+    lt9.price = "60000";
+    lt9.RAM = "12";
+    lt9.ROM = "500";
+    lt9.CPU = "Core i7";
+    lt9.screen = "IPS60";
+    lt9.GPU = "IntelUHD";
+    lt9.color = "Grey";
+
+    Catalog lt10;
+    lt10.id = 10;
+    lt10.brand = "Asus";
+    lt10.price = "110000";
+    lt10.RAM = "16";
+    lt10.ROM = "750";
+    lt10.CPU = "Ryzen 9";
+    lt10.screen = "IPS144";
+    lt10.GPU = "RTX2080";
+    lt10.color = "Black";
+    }
+}
