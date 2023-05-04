775ce675a4d26667b930226bf3b0def89003c05f
DanyaKachimati <myclearsoap0369@gmail.com>
2023-05-04 21:15:15 +0400

    l2edit

--- JavaSeminarProject/src/main/java/lesson3/Main.java
+++ JavaSeminarProject/src/main/java/lesson3/Main.java
@@ -1,4 +1,4 @@
-package lesson3;
-
-public class Main {
-}
+//package lesson3;
+//
+//public class Main {
+//}
--- JavaSeminarProject/src/main/java/lesson6_final/Laptops.java
+++ JavaSeminarProject/src/main/java/lesson6_final/Laptops.java
@@ -1,19 +1,19 @@
-package lesson6_final;
-
-public class Laptops {
-    public static void main(String[] args) {
-        Catalog lt1 = new Catalog();
-        Catalog lt2 = new Catalog();
-        Catalog lt3 = new Catalog();
-        Catalog lt4 = new Catalog();
-        Catalog lt5 = new Catalog();
-        Catalog lt6 = new Catalog();
-        Catalog lt7 = new Catalog();
-        Catalog lt8 = new Catalog();
-        Catalog lt9 = new Catalog();
-        Catalog lt10 = new Catalog();
-
-
-        System.out.println(lt1);
-    }
-}
+//package lesson6_final;
+//
+//public class Laptops {
+//    public static void main(String[] args) {
+//        Catalog lt1 = new Catalog();
+//        Catalog lt2 = new Catalog();
+//        Catalog lt3 = new Catalog();
+//        Catalog lt4 = new Catalog();
+//        Catalog lt5 = new Catalog();
+//        Catalog lt6 = new Catalog();
+//        Catalog lt7 = new Catalog();
+//        Catalog lt8 = new Catalog();
+//        Catalog lt9 = new Catalog();
+//        Catalog lt10 = new Catalog();
+//
+//
+//        System.out.println(lt1);
+//    }
+//}
