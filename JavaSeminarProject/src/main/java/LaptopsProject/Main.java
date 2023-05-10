96d75dccfde52690c6d86defc573ca13925d116d
DanyaKachimati <myclearsoap0369@gmail.com>
2023-05-10 04:19:02 +0400

    lapt_edit

--- JavaSeminarProject/src/main/java/LaptopsProject/Main.java
+++ JavaSeminarProject/src/main/java/LaptopsProject/Main.java
@@ -1,5 +1,15 @@
 package LaptopsProject;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        laptops();
+    }
+
+    private static void laptops() {
+        List<ArrayList> laptAl = new ArrayList<>();
+        System.out.println(laptAl);
+    }
 }
